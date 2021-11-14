b6cbd3d62a71757fb9b0e3bc85abbb70c65bb421
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2021-11-14 11:18:50 -0800

    Create kthLargestNumber.java

---
+++ Heap/kthLargestNumber.java
@@ -0,0 +1,52 @@
+/**
+1985. Find the Kth Largest Integer in the Array
+
+You are given an array of strings nums and an integer k. Each string in nums represents an integer without leading zeros.
+
+Return the string that represents the kth largest integer in nums.
+
+Note: Duplicate numbers should be counted distinctly. For example, if nums is ["1","2","2"], "2" is the first largest integer, "2" is the second-largest integer, and "1" is the third-largest integer.
+
+ 
+
+Example 1:
+
+Input: nums = ["3","6","7","10"], k = 4
+Output: "3"
+Explanation:
+The numbers in nums sorted in non-decreasing order are ["3","6","7","10"].
+The 4th largest integer in nums is "3".
+Example 2:
+
+Input: nums = ["2","21","12","1"], k = 3
+Output: "2"
+Explanation:
+The numbers in nums sorted in non-decreasing order are ["1","2","12","21"].
+The 3rd largest integer in nums is "2".
+Example 3:
+
+Input: nums = ["0","0"], k = 2
+Output: "0"
+Explanation:
+The numbers in nums sorted in non-decreasing order are ["0","0"].
+The 2nd largest integer in nums is "0".
+**/
+
+class Solution {
+    public String kthLargestNumber(String[] nums, int k) {
+        PriorityQueue<String> minHeap = new PriorityQueue<>((s1, s2) -> {
+            if (s1.length() == s2.length()) 
+                return s1.compareTo(s2);
+            else
+                return Integer.compare(s1.length(), s2.length());
+        });
+        int n = nums.length;
+        Arrays.stream(nums)
+            .forEach(num -> {
+                minHeap.add(num);
+            if(minHeap.size() > k) minHeap.poll();
+            });
+        
+        return minHeap.poll();
+    }
+}
