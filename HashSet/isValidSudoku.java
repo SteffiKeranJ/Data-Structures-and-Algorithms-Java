a0e798eaa634ba7e29e7b7af149ea959e178d5fb
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2023-09-24 23:09:37 +0530

    Valid Sudoku
    https://leetcode.com/problems/valid-sudoku/

---
+++ HashSet/isValidSudoku.java
@@ -0,0 +1,84 @@
+/**
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+    Each row must contain the digits 1-9 without repetition.
+    Each column must contain the digits 1-9 without repetition.
+    Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+
+    A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+    Only the filled cells need to be validated according to the mentioned rules.
+
+ 
+
+Example 1:
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+ 
+
+Constraints:
+
+    board.length == 9
+    board[i].length == 9
+    board[i][j] is a digit 1-9 or '.'.
+
+
+*/
+
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> uniqueSet = new HashSet<>();
+        int i, j;
+        for(i = 0; i < 9; ++i) {
+            for(j = 0; j < 9; ++j) {
+                if(board[i][j] == '.') continue;
+
+                char cell = board[i][j];
+
+                if(
+                    uniqueSet.contains(cell + "r" + i) ||
+                    uniqueSet.contains(cell + "c" + j) ||
+                    uniqueSet.contains(cell + "b" + i/3+j/3)
+                )
+                    return false;
+                
+                uniqueSet.addAll(Arrays.asList(
+                    cell + "r" + i, 
+                    cell + "c" + j, 
+                    cell + "b" + i/3+j/3)
+                );
+            }
+        }
+
+        return true;
+    }
+}
