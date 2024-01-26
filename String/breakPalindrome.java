f66a530bc4bc23e059d7d5d735ced0a44cd4d148
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2024-01-26 17:13:21 +0530

    1328. Break a Palindrome
    https://leetcode.com/problems/break-a-palindrome

---
+++ String/breakPalindrome.java
@@ -0,0 +1,41 @@
+/**
+Given a palindromic string of lowercase English letters palindrome, replace exactly one character with any lowercase English letter so that the resulting string is not a palindrome and that it is the lexicographically smallest one possible.
+
+Return the resulting string. If there is no way to replace a character to make it not a palindrome, return an empty string.
+
+A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, a has a character strictly smaller than the corresponding character in b. For example, "abcc" is lexicographically smaller than "abcd" because the first position they differ is at the fourth character, and 'c' is smaller than 'd'.
+
+Example 1:
+
+Input: palindrome = "abccba"
+Output: "aaccba"
+Explanation: There are many ways to make "abccba" not a palindrome, such as "zbccba", "aaccba", and "abacba".
+Of all the ways, "aaccba" is the lexicographically smallest.
+
+Example 2:
+
+Input: palindrome = "a"
+Output: ""
+Explanation: There is no way to replace a single character to make "a" not a palindrome, so return an empty string.
+**/
+
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int l = palindrome.length();
+        if(l==1) return "";
+        StringBuilder result = new StringBuilder(palindrome);
+
+        char aChar = 'a';
+
+        for(int i=0; i<l/2; ++i){
+            char ch = result.charAt(i);
+            if(ch!=aChar) {                
+                result.setCharAt(i,aChar);
+                return result.toString();
+            }
+        }
+        result.setCharAt(l-1, 'b');
+        
+        return result.toString();
+    }
+}
