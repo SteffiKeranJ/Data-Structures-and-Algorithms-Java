7497541151315a5e2c6c0abcf903b78acfdd13b1
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2024-04-20 23:24:46 +0530

    35. Search Insert Position
    https://leetcode.com/problems/search-insert-position/description/

--- Binary Search/Search2.java
+++ Binary Search/Search2.java
@@ -1,6 +1,5 @@
 /*
-Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
-If target exists, then return its index. Otherwise, return -1.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You must write an algorithm with O(log n) runtime complexity.
 
@@ -8,30 +7,32 @@ You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 
-Input: nums = [-1,0,3,5,9,12], target = 9
-Output: 4
-Explanation: 9 exists in nums and its index is 4
+Input: nums = [1,3,5,6], target = 5
+Output: 2
 
 Example 2:
 
-Input: nums = [-1,0,3,5,9,12], target = 2
-Output: -1
-Explanation: 2 does not exist in nums so return -1
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
 
 */
-
 class Solution {
-    public int search(int[] nums, int target) {
-        int right = nums.length - 1;
-        int left = 0, mid;
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length-1, mid;
 
         while(left <= right) {
             mid = left + right >> 1;
+
             if(nums[mid] == target) return mid;
-            if(nums[mid] < target) left = mid + 1;
+            if(nums[mid] < target) left = mid+1;
             else right = mid - 1;
         }
-        return -1;
-
+        return left;
     }
 }
