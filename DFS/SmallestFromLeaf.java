71f55a845f234170ecbcfb7132fef719dd494f04
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2024-05-20 10:57:36 +0530

    988. Smallest String Starting From Leaf
    https://leetcode.com/problems/smallest-string-starting-from-leaf/description/

---
+++ DFS/SmallestFromLeaf.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    String smallestString = "";
+    public String smallestFromLeaf(TreeNode root) {
+        dfs(root, "");
+        return smallestString;        
+    }
+
+    void dfs(TreeNode root, String curr) {
+        if(root == null) return;
+        curr = (char)(root.val + 'a') + curr;
+        if(root.left == null && root.right == null) {
+            if(smallestString.isEmpty() || smallestString.compareTo(curr) > 0) {
+                smallestString = curr;
+            }
+        }
+        if(root.left != null) dfs(root.left, curr);
+        if(root.right != null) dfs(root.right, curr);
+
+    }
+
+}
