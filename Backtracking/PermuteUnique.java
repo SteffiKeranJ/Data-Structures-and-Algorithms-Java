fa9ba00db606a87314d7a30bf52bcf381357b265
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2025-03-23 16:46:25 +0530

    Update PermuteUnique.java

--- Backtracking/PermuteUnique.java
+++ Backtracking/PermuteUnique.java
@@ -16,34 +16,25 @@ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 **/
 
 class Solution {
-    static List<List<Integer>> ans;
-    static int l;
-
-    private static void swap(int[] nums, int i, int j) {
-        int a = nums[i];
-        nums[i] = nums[j];
-        nums[j] = a;
-    }
-
-    private static void helper(int[] nums, int idx) {
-        if(idx == l-1) {
-            ans.add(Arrays.stream(nums).boxed().toList());
-            return;
-        }
-        HashSet<Integer> uniqueSet = new HashSet<>();
-        for(int i = idx; i < l; ++i) {
-            if(uniqueSet.contains(nums[i])) continue;
-            uniqueSet.add(nums[i]);
-            swap(nums, i, idx);
-            helper(nums, idx+1);
-            swap(nums, i, idx);
-        }
-    }
+    List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> permuteUnique(int[] nums) {
-        ans = new ArrayList<>();
-        l = nums.length;
-        helper(nums, 0);
-        return ans;
+        Arrays.sort(nums);
+        backTrack(nums, new ArrayList<Integer>(), new boolean[nums.length]);
+        return result;
+    }
+
+    private void backTrack(int[] nums, List<Integer> list, boolean[] used) {
+        if(list.size() == nums.length) result.add(new ArrayList<>(list));
+        for(int i = 0; i < nums.length; ++i) {
+            if(used[i]) continue;
+            // to skip dups
+            if(i > 0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+            list.add(nums[i]);
+            used[i] = true;
+            backTrack(nums, list, used);
+            used[i] = false;
+            list.remove(list.size()-1);
+        }
     }
 }
